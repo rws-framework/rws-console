0597ffbda4b5048ac1a59b33494a50ea679a3341
root <root@BlackSUPREME>
2024-03-19 18:54:09 +0100

    v0.1.13

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,12 +1,12 @@
 import * as rwsShell from './tools/shell';
 import * as rwsPath from './tools/path';
 import * as rwsFS from './tools/fs';
-import rwsArgsHelper from './helpers/_run';
+import rwsArgsHelper, { RWSInputType } from './helpers/_run';
 import { RWSManagedConsole, loadAction, bootstrap, RWSCliBootstrap } from './helpers/_managed_console';
 declare const rwsCli: {
     loadAction: typeof loadAction;
     bootstrap: typeof bootstrap;
     RWSCliBootstrap: typeof RWSCliBootstrap;
 };
-export { rwsShell, rwsPath, rwsFS, RWSManagedConsole, rwsArgsHelper, rwsCli };
+export { rwsShell, rwsPath, rwsFS, RWSManagedConsole, rwsArgsHelper, rwsCli, RWSInputType };
 export default rwsCli;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as rwsShell from './tools/shell';
 import * as rwsPath from './tools/path';
 import * as rwsFS from './tools/fs';
-import rwsArgsHelper  from './helpers/_run';
+import rwsArgsHelper, {RWSInputType}  from './helpers/_run';
 
 import { RWSManagedConsole, loadAction, bootstrap, RWSCliBootstrap } from './helpers/_managed_console';
 
@@ -15,7 +15,8 @@ export {
     rwsFS,
     RWSManagedConsole,
     rwsArgsHelper,
-    rwsCli
+    rwsCli,
+    RWSInputType
 };
 
 export default rwsCli;
